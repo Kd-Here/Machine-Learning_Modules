b863f9b55d8e9f416cb3f9896a40667fe6def34f
Kd-Here <gamelover9899@gmail.com>
2022-09-14 11:25:26 +0530

    Python checked opencv package

--- Numpy/Join_Split.py
+++ Numpy/Join_Split.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+import cv2
+print("To check version of opencv",cv2.__version__)
+
+
+
 arr1 = np.array([1, 2, 3])
 arr2 = np.array([4, 5, 6])
 arr = np.concatenate((arr1, arr2))
